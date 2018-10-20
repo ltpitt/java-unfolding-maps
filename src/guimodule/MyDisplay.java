abdd1aa91dfbd86c817b7a6716cb1bb0472c1031
ltpitt <d.nastri@gmail.com>
2018-10-20 21:52:50 +0200

    feat: Experimenting with basic shapes in MyDisplay class

--- src/guimodule/MyDisplay.java
+++ src/guimodule/MyDisplay.java
@@ -10,7 +10,13 @@ public class MyDisplay extends PApplet {
     }
 
     public void draw(){
-
+        fill(255,255,0);
+        ellipse(200, 200, 390,390);
+        fill(0,0,0);
+        ellipse(120,130,50,70);
+        ellipse(280, 130,50,70);
+        arc(200,280,75,75,0,PI);
+        noFill();
     }
 
 
