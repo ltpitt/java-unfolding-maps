185bd90120285cdbc26016f32d475dad3448b223
Davide Nastri <d.nastri@gmail.com>
2019-04-20 00:18:46 +0200

    feat: Implemented module5 Classes

--- src/module5/CommonMarker.java
+++ src/module5/CommonMarker.java
@@ -5,7 +5,7 @@ import de.fhpotsdam.unfolding.marker.SimplePointMarker;
 import processing.core.PGraphics;
 
 /** Implements a common marker for cities and earthquakes on an earthquake map
- * 
+ *
  * @author UC San Diego Intermediate Software Development MOOC team
  * @author Your name here
  *
@@ -14,25 +14,25 @@ public abstract class CommonMarker extends SimplePointMarker {
 
 	// Records whether this marker has been clicked (most recently)
 	protected boolean clicked = false;
-	
+
 	public CommonMarker(Location location) {
 		super(location);
 	}
-	
+
 	public CommonMarker(Location location, java.util.HashMap<java.lang.String,java.lang.Object> properties) {
 		super(location, properties);
 	}
-	
+
 	// Getter method for clicked field
 	public boolean getClicked() {
 		return clicked;
 	}
-	
+
 	// Setter method for clicked field
 	public void setClicked(boolean state) {
 		clicked = state;
 	}
-	
+
 	// Common piece of drawing method for markers; 
 	// Note that you should implement this by making calls 
 	// drawMarker and showTitle, which are abstract methods 
