ea19cced8c4091418fd1356af0de31023eebff3a
Davide Nastri <d.nastri@gmail.com>
2019-05-24 22:09:37 +0200

    feat: Added addCityKey() to show city details

--- src/module6/EarthquakeCityMap.java
+++ src/module6/EarthquakeCityMap.java
@@ -2,7 +2,6 @@ package module6;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 import de.fhpotsdam.unfolding.UnfoldingMap;
@@ -13,8 +12,8 @@ import de.fhpotsdam.unfolding.geo.Location;
 import de.fhpotsdam.unfolding.marker.AbstractShapeMarker;
 import de.fhpotsdam.unfolding.marker.Marker;
 import de.fhpotsdam.unfolding.marker.MultiMarker;
-import de.fhpotsdam.unfolding.providers.Google;
 import de.fhpotsdam.unfolding.providers.MBTilesMapProvider;
+import de.fhpotsdam.unfolding.providers.Microsoft;
 import de.fhpotsdam.unfolding.utils.MapUtils;
 import parsing.ParseFeed;
 import processing.core.PApplet;
@@ -65,6 +64,7 @@ public class EarthquakeCityMap extends PApplet {
 	// NEW IN MODULE 5
 	private CommonMarker lastSelected;
 	private CommonMarker lastClicked;
+	private Boolean isCityDetailShown;
 	
 	public void setup() {		
 		// (1) Initializing canvas and map tiles
@@ -74,7 +74,7 @@ public class EarthquakeCityMap extends PApplet {
 		    earthquakesURL = "2.5_week.atom";  // The same feed, but saved August 7, 2015
 		}
 		else {
-			map = new UnfoldingMap(this, 200, 50, 650, 600, new Google.GoogleMapProvider());
+			map = new UnfoldingMap(this, 200, 10, 700, 500, new Microsoft.HybridProvider());
 			// IF YOU WANT TO TEST WITH A LOCAL FILE, uncomment the next line
 		    //earthquakesURL = "2.5_week.atom";
 		}
@@ -83,10 +83,10 @@ public class EarthquakeCityMap extends PApplet {
 		// FOR TESTING: Set earthquakesURL to be one of the testing files by uncommenting
 		// one of the lines below.  This will work whether you are online or offline
 		//earthquakesURL = "test1.atom";
-		earthquakesURL = "test2.atom";
+		//earthquakesURL = "test2.atom";
 		
 		// Uncomment this line to take the quiz
-		//earthquakesURL = "quiz2.atom";
+		earthquakesURL = "quiz2.atom";
 		
 		
 		// (2) Reading in earthquake data and geometric properties
@@ -134,6 +134,7 @@ public class EarthquakeCityMap extends PApplet {
 		background(0);
 		map.draw();
 		addKey();
+		addCityKey();
 		
 	}
 	
@@ -215,11 +216,14 @@ public class EarthquakeCityMap extends PApplet {
 	// and respond appropriately
 	private void checkCitiesForClick()
 	{
-		if (lastClicked != null) return;
+		if (lastClicked != null) {
+		    return;
+        }
 		// Loop over the earthquake markers to see if one of them is selected
 		for (Marker marker : cityMarkers) {
 			if (!marker.isHidden() && marker.isInside(map, mouseX, mouseY)) {
 				lastClicked = (CommonMarker)marker;
+				isCityDetailShown = Boolean.TRUE;
 				// Hide all the other earthquakes and hide
 				for (Marker mhide : cityMarkers) {
 					if (mhide != lastClicked) {
@@ -274,52 +278,52 @@ public class EarthquakeCityMap extends PApplet {
 		for(Marker marker : cityMarkers) {
 			marker.setHidden(false);
 		}
+        isCityDetailShown = Boolean.FALSE;
 	}
 	
 	// helper method to draw key in GUI
 	private void addKey() {	
 		// Remember you can use Processing's graphics methods here
 		fill(255, 250, 240);
-		
+
 		int xbase = 25;
 		int ybase = 50;
-		
+
 		rect(xbase, ybase, 150, 250);
-		
+
 		fill(0);
 		textAlign(LEFT, CENTER);
 		textSize(12);
 		text("Earthquake Key", xbase+25, ybase+25);
-		
+
 		fill(150, 30, 30);
 		int tri_xbase = xbase + 35;
 		int tri_ybase = ybase + 50;
-		triangle(tri_xbase, tri_ybase-CityMarker.TRI_SIZE, tri_xbase-CityMarker.TRI_SIZE, 
-				tri_ybase+CityMarker.TRI_SIZE, tri_xbase+CityMarker.TRI_SIZE, 
+		triangle(tri_xbase, tri_ybase-CityMarker.TRI_SIZE, tri_xbase-CityMarker.TRI_SIZE,
+				tri_ybase+CityMarker.TRI_SIZE, tri_xbase+CityMarker.TRI_SIZE,
 				tri_ybase+CityMarker.TRI_SIZE);
 
 		fill(0, 0, 0);
 		textAlign(LEFT, CENTER);
 		text("City Marker", tri_xbase + 15, tri_ybase);
-		
-		text("Land Quake", xbase+50, ybase+70);
+		text("Land Quake ", xbase+50, ybase+70);
 		text("Ocean Quake", xbase+50, ybase+90);
 		text("Size ~ Magnitude", xbase+25, ybase+110);
-		
+
 		fill(255, 255, 255);
-		ellipse(xbase+35, 
-				ybase+70, 
-				10, 
+		ellipse(xbase+35,
+				ybase+70,
+				10,
 				10);
 		rect(xbase+35-5, ybase+90-5, 10, 10);
-		
+
 		fill(color(255, 255, 0));
 		ellipse(xbase+35, ybase+140, 12, 12);
 		fill(color(0, 0, 255));
 		ellipse(xbase+35, ybase+160, 12, 12);
 		fill(color(255, 0, 0));
 		ellipse(xbase+35, ybase+180, 12, 12);
-		
+
 		textAlign(LEFT, CENTER);
 		fill(0, 0, 0);
 		text("Shallow", xbase+50, ybase+140);
@@ -327,7 +331,7 @@ public class EarthquakeCityMap extends PApplet {
 		text("Deep", xbase+50, ybase+180);
 
 		text("Past hour", xbase+50, ybase+200);
-		
+
 		fill(255, 255, 255);
 		int centerx = xbase+35;
 		int centery = ybase+200;
@@ -340,7 +344,33 @@ public class EarthquakeCityMap extends PApplet {
 		
 	}
 
-	
+	private void addCityKey() {
+		if (isCityDetailShown == Boolean.TRUE) {
+			// Remember you can use Processing's graphics methods here
+			fill(255, 250, 240);
+
+			int xbase = 25;
+			int ybase = 350;
+
+			rect(xbase, ybase, 150, 250);
+
+			fill(0);
+			textAlign(LEFT, CENTER);
+			textSize(12);
+			text("City Key", xbase + 25, ybase + 25);
+
+			fill(0, 0, 0);
+			textAlign(LEFT, CENTER);
+			text("Name: " + lastClicked.getProperties().get("name"), xbase + 25, ybase + 50);
+			text("Country: " + lastClicked.getProperties().get("country"), xbase + 25, ybase + 70);
+			text("Population: " + lastClicked.getProperties().get("population"), xbase + 25, ybase + 90);
+			String coastal = lastClicked.getProperties().get("coastal").equals("true") ? "Yes" : "no";
+			text("Coastal: " + coastal, xbase + 25, ybase + 110);
+
+		}
+
+	}
+
 	
 	// Checks whether this quake occurred on land.  If it did, it sets the 
 	// "country" property of its PointFeature to the country where it occurred
